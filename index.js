6712da058fa4871c4d860d02320527dac6132dea
clinton wambugu <clintonwambugu@gmail.com>
2018-09-26 22:36:50 +0300

    worked on laying out the dipslay and btn div

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function multiply (a,b) {
 function divide(a,b){
     return a/b;
 }
+
+function operator(sign,a,b){
+		
+	switch (sign) {
+		case '+':
+			add(a,b)
+			break;
+		case '/':
+			divide(a,b)
+			break;
+		case '-':
+			subtract(a,b)
+			break;
+		case '*':
+			multipy(a,b)
+			break;
+		default:
+			console.log('check if u included he operator');
+			break;
+	}
+}
